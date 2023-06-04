02591788f547a92d818abb9bdea8c2aecf1007cc
Wvaviator <wvaviator@gmail.com>
2023-06-04 09:22:56 -0500

    initial project setup

---
+++ src/main/java/com/wvaviator/greenkeep/GreenkeepApplication.java
@@ -0,0 +1,13 @@
+package com.wvaviator.greenkeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenkeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenkeepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wvaviator/greenkeep/GreenkeepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wvaviator.greenkeep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenkeepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
