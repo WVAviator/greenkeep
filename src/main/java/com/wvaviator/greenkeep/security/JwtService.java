a9aacab5f2312766d6339e7fbe051bbdc6cc0405
Wvaviator <wvaviator@gmail.com>
2023-06-15 16:34:23 -0500

    set up jwt filter and service

---
+++ src/main/java/com/wvaviator/greenkeep/security/JwtService.java
@@ -0,0 +1,11 @@
+package com.wvaviator.greenkeep.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractEmail(String jwt) {
+        return null;
+    }
+}
